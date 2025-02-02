b170f6bc35bba51f939804cfbe08199baa378d74
Nourish Cherish <ncherish@uwaterloo.ca>
2025-02-01 19:36:11 -0500

    image

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -6,50 +6,43 @@ import { OpenAI } from "openai";
 
 const apikey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({
-  apiKey: apikey,
+	apiKey: apikey,
 });
 
 export async function POST(req: Request) {
-  try {
-    const { userId } = auth();
-    const body = await req.json();
-    const { prompt, amount = 1, resolution = "1024x1024" } = body;
+	try {
+		const { userId } = auth();
+		const body = await req.json();
+		const { prompt, amount = 1, resolution = "1024x1024" } = body;
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
 
-    if (!apikey) {
-      return new NextResponse("Missing OpenAI API Key", { status: 500 });
-    }
+		if (!apikey) {
+			return new NextResponse("Missing OpenAI API Key", { status: 500 });
+		}
 
-    if (!prompt) {
-      return new NextResponse("Prompt is required ", { status: 400 });
-    }
-    if (!amount) {
-      return new NextResponse("amount is required ", { status: 400 });
-    }
-    if (!resolution) {
-      return new NextResponse("resolution is required ", { status: 400 });
-    }
-    const freeTrial = await checkApiLimit();
-    const isPro = await checkSubscription();
-    if (!freeTrial && !isPro) {
-      return new NextResponse("Free trial has expire", { status: 403 });
-    }
-    const response = await openai.images.generate({
-      model: "dall-e-3",
-      prompt: prompt,
-      n: 1,
-      size: resolution,
-    });
-    if (!isPro) {
-      await increaseApiLimit();
-    }
+		if (!prompt) {
+			return new NextResponse("Prompt is required ", { status: 400 });
+		}
+		if (!amount) {
+			return new NextResponse("amount is required ", { status: 400 });
+		}
+		if (!resolution) {
+			return new NextResponse("resolution is required ", { status: 400 });
+		}
 
-    console.log("response tada");
-    return NextResponse.json(response.data);
-  } catch (err) {
-    return new NextResponse("Internal Server Error");
-  }
+		const response = await openai.images.generate({
+			model: "dall-e-3",
+			prompt: prompt,
+			n: 1,
+			size: resolution,
+		});
+
+		console.log("response tada");
+		return NextResponse.json(response.data);
+	} catch (err) {
+		return new NextResponse("Internal Server Error");
+	}
 }
